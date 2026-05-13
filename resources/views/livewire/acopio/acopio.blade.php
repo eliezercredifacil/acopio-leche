fbdeb1ad213834f24db8bec97f93c6c1d2eca945
Natan Siria <eliezer.credifacil@gmail.com>
2026-05-13 00:46:47 -0600

    Apariencia de tabla acopios

--- resources/views/livewire/acopio/acopio.blade.php
+++ resources/views/livewire/acopio/acopio.blade.php
@@ -10,7 +10,7 @@
 
         @foreach ($localidades as $localidad)
         <button wire:click="$set('localidadId', {{ $localidad->id }})"
-            class="px-4 py-2 rounded 
+            class="px-4 py-2 rounded btn btn-sm
                 {{ $localidadId == $localidad->id ? 'bg-blue-600 text-white' : 'bg-gray-200 dark:bg-gray-700' }}">
             {{ $localidad->nombre }}
         </button>
@@ -18,47 +18,71 @@
 
     </div>
 
-    <table class="table min-w-full border-collapse">
+    <div class="overflow-x-auto">
 
-        <thead>
+        <table class="table min-w-full border-collapse">
+
+            <thead>
+                <tr>
+                    <th class="bg-cyan-800 text-white border border-gray-300">Nombres</th>
+
+                    @foreach ($fechas as $fecha)
+                    <th class="bg-lime-700 text-white border text-center border-gray-300">
+                        {{ $fecha->translatedFormat('D d') }}
+                    </th>
+                    @endforeach
+
+                    <th class="border border-gray-300">Total entregados</th>
+                    <th class="border border-gray-300">Precio litro</th>
+                    <th class="border border-gray-300">Total córdobas</th>
+                    <th class="border border-gray-300">% deducción compra</th>
+
+                    <th class="border border-gray-300">Total entregados</th>
+                    <th class="border border-gray-300">Precio litro</th>
+                    <th class="border border-gray-300">Total córdobas</th>
+                    <th class="border border-gray-300">% deducción compra</th>
+
+                    <th class="border border-gray-300">Precio litro</th>
+                    <th class="border border-gray-300">Total córdobas</th>
+                    <th class="border border-gray-300">% deducción compra</th>
+                </tr>
+            </thead>
+
+            @foreach ($this->productores as $productor)
             <tr>
-                <th>Nombres</th>
+                <td class="bg-gray-600 text-white sticky left-0 z-10 border border-gray-300 whitespace-nowrap">{{ $productor->nombre }}</td>
 
                 @foreach ($fechas as $fecha)
-                <th>
-                    {{ $fecha->translatedFormat('D d') }}
-                </th>
+                @php
+                $acopio = $this->acopiosMap[$productor->id][$fecha->toDateString()] ?? null;
+                @endphp
+
+                <td class="border border-gray-300 text-center align-middle">
+                    <input type="number" value="{{ $acopio->litros ?? '' }}"
+                        wire:blur="guardar({{ $productor->id }}, '{{ $fecha->toDateString() }}', $event.target.value)"
+                        class="w-12 text-center bg-base-100 border border-gray-500 rounded-none p-0">
+                </td>
                 @endforeach
 
-                <th>Total entregados</th>
-                <th>Precio litro</th>
+                <td class="border border-gray-300 text-center"></td>
+                <td class="border border-gray-300 text-center"></td>
+                <td class="border border-gray-300 text-center"></td>
+                <td class="border border-gray-300 text-center"></td>
+
+                <td class="border border-gray-300 text-center"></td>
+                <td class="border border-gray-300 text-center"></td>
+                <td class="border border-gray-300 text-center"></td>
+                <td class="border border-gray-300 text-center"></td>
+
+                <td class="border border-gray-300 text-center"></td>
+                <td class="border border-gray-300 text-center"></td>
+                <td class="border border-gray-300 text-center"></td>
             </tr>
-        </thead>
-
-        @foreach ($this->productores as $productor)
-        <tr>
-            <td class="border border-gray-300 text-sm whitespace-nowrap">{{ $productor->nombre }}</td>
-
-            @foreach ($fechas as $fecha)
-            @php
-            $acopio = $this->acopiosMap[$productor->id][$fecha->toDateString()] ?? null;
-            @endphp
-
-            <td class="border border-gray-300 text-sm text-center align-middle p-0">
-                <input
-                    type="number"
-                    value="{{ $acopio->litros ?? '' }}"
-                    wire:blur="guardar({{ $productor->id }}, '{{ $fecha->toDateString() }}', $event.target.value)"
-                    class="w-full h-7 text-center text-sm bg-base-100 border border-gray-500 rounded-none p-0">
-            </td>
             @endforeach
 
-            <td class="border border-gray-300 text-sm text-center"></td>
-            <td class="border border-gray-300 text-sm text-center"></td>
-        </tr>
-        @endforeach
+        </table>
 
-    </table>
+    </div>
 
 
 </div>
