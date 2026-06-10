4a7833ba839d902f97cc26d28f410f3ea8b5bae2
Eliezer <eliezer.siria@gmail.com>
2026-06-10 10:03:33 -0600

    Se agrega modifica menu resp

--- resources/views/components/acopio/menu-acopio.blade.php
+++ resources/views/components/acopio/menu-acopio.blade.php
@@ -25,24 +25,21 @@
     <ul class="menu menu-horizontal bg-base-200 rounded-box">
         <li>
             <a href="{{ route('acopio') }}" role="tab"
-                class="tab border-2 border-solid {{ request()->routeIs('acopio') ? 'bg-sky-800 font-bold text-white' : '' }}">
-                <i class="fa-solid fa-chart-line"></i>
-                Reporte de esta semana
+                class="tab border-2 border-solid {{ request()->routeIs('acopio') ? 'bg-sky-800 font-bold text-white' : '' }}">                
+                Reporte
             </a>
         </li>
 
         <li>
             <a href="{{ route('acopio.resumen-semanal') }}" role="tab"
-                class="tab border-2 border-solid {{ request()->routeIs('acopio.resumen-semanal') ? 'bg-sky-800 font-bold text-white' : '' }}">
-                <i class="fa-solid fa-chart-simple"></i>
-                Resumen semanal
+                class="tab border-2 border-solid {{ request()->routeIs('acopio.resumen-semanal') ? 'bg-sky-800 font-bold text-white' : '' }}">                
+                Resumen
             </a>
         </li>
 
         <li>
             <a href="{{ route('acopio.recibos') }}" role="tab"
-                class="tab border-2 border-solid {{ request()->routeIs('acopio.recibos') ? 'bg-sky-800 font-bold text-white' : '' }}">
-                <i class="fa-solid fa-file-invoice-dollar"></i>
+                class="tab border-2 border-solid {{ request()->routeIs('acopio.recibos') ? 'bg-sky-800 font-bold text-white' : '' }}">                
                 Recibos
             </a>
         </li>
