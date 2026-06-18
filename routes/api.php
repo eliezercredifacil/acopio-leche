6b54dd4b55fc4afdc34277f6634ecb0ea5cf2f51
Eliezer <eliezer.siria@gmail.com>
2026-06-17 23:34:30 -0600

    Se borra la ruta la palabra api

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\ProductorController;
 
-Route::post('api/login', [AuthController::class, 'login']);
+Route::post('login', [AuthController::class, 'login']);
 
-Route::get('api/productores', [ProductorController::class, 'index']);
+Route::get('productores', [ProductorController::class, 'index']);
